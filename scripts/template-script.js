0a24183faeeb61c21911220d4dabfb493bf678a4
Reth Ramos <rethanielramos@gmail.com>
2020-03-03 00:13:46 +0800

    moved files from assets and scripts directories to public directory

--- scripts/template-script.js
+++
@@ -1,9 +0,0 @@
-// Load the template from template.html to class .template
-$(document).ready(function () {
-    var href = window.location.href;
-    var template = (href.search('-mod') == -1) ? 'template.html' : 'template-mod.html';
-    $('#template').load(template);
-
-    $('footer').html('<p>© CCAPDEV X22 Team 3 <br> Dela Cruz, Gubat, Ramos</p>');
-
-});
