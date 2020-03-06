7a55c6f68d248408b8491fe128cbd471ff8e6fb2
Reth Ramos <rethanielramos@gmail.com>
2020-03-07 02:41:57 +0800

    modified responsiveFontSize(), added active()
    responsiveFontSize() now takes at most 2 parameters, the first being the selector string and the last an optional size that overrides any 'res-n' declared sizes in an element's 'res-n' class. The function active() takes in a  selector for a <ul> element  as a parameter and decorates the active page's <a> element.

--- public/javascript/styling-script.js
+++ public/javascript/styling-script.js
@@ -30,22 +30,39 @@ var href = document.location.href;
 //     list[0].firstElementChild.id = 'active';
 // }
 
+function active(selector) {
+    var list = document.querySelector(selector).children;
+    for (let i = 0; i < list.length; i++) {
+        const element = list[i];
+        var route = element.firstElementChild.getAttribute('href').slice(1);
+        console.log(element);
+        if (window.location.href.includes(route)) {
+            element.firstElementChild.id = "active";
+        }
+    }
+}
 
-function responsiveFontSize() {
-    var regex = /s?/;
-    console.log(regex);
-    // var resList = document.getElementsByClassName()
-    var resList = document.querySelectorAll("[class^='res-']");
-    console.log(resList);
+function responsiveFontSize(selector) {
+    var resList = document.querySelectorAll(selector);
 
     resList.forEach(e => {
-        var regex = /\d+/;
-        var num = (e.className.match(regex)[0]) / 2;
+        var num;
+        if (arguments.length == 1) {
+            var regex = /\d+/;
+            num = (e.className.match(regex)[0]) / 2;
+        }
+        else {
+            num = arguments[1] / 2;
+        }
 
-        // console.log(num);
         e.style.fontSize = "calc(" + (num / 16) + "rem + " + ((num / 1200) * 100) + "vw)";
-        console.log(e.style.fontSize);
     });
 }
 
-responsiveFontSize();
+responsiveFontSize("[class^='res-']");
+responsiveFontSize("h1", 60);
+responsiveFontSize("h2", 42);
+responsiveFontSize("h3", 20);
+
+active("#navlist");
+active("#sidebarlist")
