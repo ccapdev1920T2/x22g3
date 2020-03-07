baefc301d99926a1e8617a16be3cd6f433da91d5
Reth Ramos <rethanielramos@gmail.com>
2020-03-07 20:18:44 +0800

    routing changes
    Included body-parser as module, moved '/' to indexController router and '/enrollment' to enrollmentController router, added POST method for /login route

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ var port = 8080;
 var express = require('express');
 var app = express();
 const hbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+// Routes
+var indexController = require('./controllers/indexController');
+var enrollmentController = require('./controllers/enrollmentController');
+
+
+app.use('/', indexController);
+app.use('/enrollment', enrollmentController);
 
 app.engine('hbs', hbs({ extname: 'hbs', defaultLayout: 'main' }));
 app.set('view engine', 'hbs');
@@ -9,9 +20,7 @@ app.set('view engine', 'hbs');
 // serve static files
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.render('index', { title: 'Home | Animo.sys' });
-});
+
 
 app.get('/login', (req, res) => {
     let view = (req.query.mod) ? 'login-mod' : 'login';
@@ -21,6 +30,24 @@ app.get('/login', (req, res) => {
         title: 'Login | Animo.sys'
     });
 });
+
+app.post('/login', urlencodedParser, (req, res) => {
+    let view = (req.query.mod) ? 'login-mod' : 'login';
+    var data = req.body;
+    var errMsg = '';
+    if (!data.idNumber || !data.password) {
+        errMsg = "Invalid ID Number/Password.";
+    }
+
+    
+    res.render(view, {
+        layout: 'sessions',
+        addedStyles: ['session-styles'],
+        title: 'Login | Animo.sys',
+        data: data,
+        errMsg: errMsg
+    });
+})
 
 app.get('/cart/:from?', (req, res) => {
     var from = req.params.from;
@@ -66,12 +93,7 @@ app.get('/preenlistment', (req, res) => {
     });
 })
 
-app.get('/enrollment', (req, res) => {
-    res.render('enrollment', {
-        title: 'Enrollment | Animo.sys',
-        addedStyles: ['enrollment-styles']
-    });
-})
+
 
 // Moderator routes
 
