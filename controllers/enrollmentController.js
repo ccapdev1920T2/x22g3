91eca96aa3f400124488d0b3d22a13fcf4b32d5c
Reth Ramos <rethanielramos@gmail.com>
2020-03-08 03:09:15 +0800

    updated  routes

--- controllers/enrollmentController.js
+++ controllers/enrollmentController.js
@@ -9,17 +9,41 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get('/classschedule', (req,res) => {
+router.get('/classschedule', (req, res) => {
     res.render('classschedule', {
         title: 'Class Schedule | Animo.sys'
     })
 })
 
-router.get('/addclass', (req,res) => {
+router.get('/addclass', (req, res) => {
     res.render('addclass', {
         title: 'Add a Class | Animo.sys'
     })
 })
 
+router.get('/dropclass', (req, res) => {
+    res.render('dropclass', {
+        title: 'Drop a Class | Animo.sys'
+    })
+})
+
+router.get('/dropclass_list', (req, res) => {
+    res.render('dropclass_list', {
+        title: 'List of Dropped Classes | Animo.sys'
+    })
+})
+
+router.get('/requestclass', (req, res) => {
+    res.render('requestclass', {
+        title: 'Request a Class | Animo.sys'
+    })
+})
+
+router.get('/courseofferings', (req, res) => {
+    res.render('courseofferings', {
+        addedStyles: ['courseofferings-styles'],
+        title: 'Course Offerings | Animo.sys'
+    })
+})
 
 module.exports = router;
