6ae0c1b98ecd497a8c3c4766ee6b521580bd8815
Reth Ramos <rethanielramos@gmail.com>
2020-03-09 23:54:16 +0800

    Moved function calls from styling-script to their corresponding layouts

--- public/javascript/styling-script.js
+++ public/javascript/styling-script.js
@@ -1,34 +1,4 @@
-// var list = document.getElementById('navlist').children;
 var href = document.location.href;
-
-// if this is a moderator page, remove margin left
-// if (href.search('-mod') != -1) {
-//     var main = document.getElementById('main');
-//     main.style.maxWidth = '100vw';
-//     var content = document.getElementById('content');
-//     content.style.marginLeft = 0;
-// } else {
-//     activate('sidebarlist');
-
-// }
-
-// activate('navlist');
-
-// give the current list item an 'active' effect
-// function activate(listSelector) {
-//     var list = document.getElementById(listSelector).children;
-//     for (let i = 0; i < list.length; i++) {
-//         var file = document.location.href.match(/[^\/]+$/)[0] || 'index.html';
-//         if (file == list[i].firstElementChild.getAttribute('href')) {
-//             list[i].firstElementChild.id = 'active';
-//         }
-//     }
-// }
-
-// For the 2 cart pages, to be refactored in Milestone 2
-// if(document.location.href.includes('cart')) {
-//     list[0].firstElementChild.id = 'active';
-// }
 
 function active(selector) {
     var list = document.querySelector(selector).children;
@@ -57,11 +27,3 @@ function responsiveFontSize(selector) {
         e.style.fontSize = "calc(" + (num / 16) + "rem + " + ((num / 1200) * 100) + "vw)";
     });
 }
-
-responsiveFontSize("[class^='res-']");
-responsiveFontSize("h1", 60);
-responsiveFontSize("h2", 42);
-responsiveFontSize("h3", 20);
-
-active("#navlist");
-active("#sidebarlist")
