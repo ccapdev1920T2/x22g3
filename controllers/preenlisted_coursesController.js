8714a03de3cb38d0f7fbdb42333698da5145b4d9
Reth Ramos <rethanielramos@gmail.com>
2020-04-01 13:25:25 +0800

    Update /preenlistment and /cart/preenlistment routes and controllers

--- controllers/preenlisted_coursesController.js
+++ controllers/preenlisted_coursesController.js
@@ -1,6 +1,17 @@
+// REQUIRE MODELS AS NEEDED
 var PreenlistedCourses = require('../models/preenlisted_courses');
+var PreenlistCourse = require('../models/preenlist_course');
 
-exports.preenlisted_courses_detail = (req, res) => {
+// Respond to /cart/preenlistment
+exports.preenlisted_courses_cart = (req, res) => {
     console.log('TODO: get data from PreenlistedCourses');
     res.render('cart_preenlistment', { title: 'Preenlistment Cart | Animo.sys' });
 };
+
+
+// Respond to /preenlistment
+exports.preenlisted_courses_list = (req, res) => {
+    res.render('preenlistment', {
+        title: "Pre-enlistment | Animo.sys"
+    });
+}
--- routes/preenlistment.js
+++ routes/preenlistment.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('preenlistment', {
-        title: "Pre-enlistment | Animo.sys"
-    });
-})
+var student_controller = require('../controllers/studentController');
+var preenlisted_courses_controller = require('../controllers/preenlisted_coursesController');
+var preenlist_course_controller = require('../controllers/preenlist_courseController');
+
+router.get('/', preenlisted_courses_controller.preenlisted_courses_list);
 
 module.exports = router;
