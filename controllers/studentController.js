98c9f245d9eb19caeefab3a782d0255ba14a9e3d
Reth Ramos <rethanielramos@gmail.com>
2020-04-02 02:29:47 +0800

    Add pdfkit as dependency

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -3,6 +3,18 @@ var Student = require('../models/student');
 var Course = require('../models/course');
 var EnrolledCourses = require('../models/enrolled_courses');
 
+var PDFDocument = require('pdfkit');
+var fs = require('fs');
+
+// TODO: implement
+function printEAF(data) {
+    const doc = new PDFDocument();
+
+    doc.pipe(fs.createWriteStream('output.pdf'));
+    doc.text('Some text', 100, 100);
+    doc.end();
+}
+
 // Display details for one student
 exports.student_detail = (req, res) => {
     console.log('TODO: get data from Student');
@@ -25,6 +37,7 @@ exports.student_class_schedule = (req, res) => {
 exports.student_add_class = (req, res) => {
     console.log('TODO: get data from Student, EnrolledCourses, and Course (to view available courses)');
     res.render('addclass', {
-        title: 'Add a Class | Animo.sys'
+        title: 'Add a Class | Animo.sys',
+        addedStyles: ['forms']
     })
 };
