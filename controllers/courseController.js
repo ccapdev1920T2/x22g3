26a51b95ca8c8da0681e4f8853b2c561858cc520
Reth Ramos <rethanielramos@gmail.com>
2020-04-02 23:34:58 +0800

    .searchbar is now in forms.css

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,12 +1,11 @@
 // REQUIRE MODELS AS NEEDED
 var Course = require('../models/course');
-var Student = require('../models/student');
 
 // display course offerings list
 exports.course_offerings_list = (req, res) => {
     console.log('TODO: get data from Course');
     res.render('courseofferings', {
-        addedStyles: ['courseofferings'],
+        addedStyles: ['forms'],
         title: 'Course Offerings | Animo.sys'
     })
 };
