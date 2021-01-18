56a7be1801bc3d07206855335d2848250876fa42
Rethaniel Ramos <rethanielramos@gmail.com>
2021-01-18 13:39:12 +0800

    use only 1 layout for both student and mod views

--- controllers/modController.js
+++ controllers/modController.js
@@ -6,8 +6,9 @@ var Student = require("../models/student");
 exports.moderator_students_list = (req, res) => {
     console.log("TODO: get data from Moderator");
     res.render("index-mod", {
-        layout: "main-mod",
-        addedStyles: ["index-mod-styles", "forms"],
+        mod: true,
+        // layout: "main-mod",
+        // addedStyles: ["index-mod-styles", "forms"],
         title: "Home | Animo.sys",
     });
 };
@@ -16,8 +17,9 @@ exports.moderator_students_list = (req, res) => {
 exports.moderator_create_student_post = (req, res) => {
     console.log("TODO: implement POST logic");
     res.render("index-mod", {
-        layout: "main-mod",
-        addedStyles: ["index-mod-styles", "forms"],
+        mod: true,
+        // layout: "main-mod",
+        // addedStyles: ["index-mod-styles", "forms"],
         data: JSON.stringify(req.body),
         title: "Home | Animo.sys",
     });
@@ -26,8 +28,9 @@ exports.moderator_create_student_post = (req, res) => {
 // Respond to /mod/settings GET request
 exports.moderator_settings_detail = (req, res) => {
     res.render("settings-mod", {
-        layout: "main-mod",
-        addedStyles: ["settings-mod-styles"],
+        mod: true,
+        // layout: "main-mod",
+        // addedStyles: ["settings-mod-styles"],
         title: "Settings | Animo.sys",
     });
 };
