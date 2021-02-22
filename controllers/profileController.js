c68d4b8ae66686373b98c0cbcd3105698d254d6b
Rethaniel Ramos <rethanielramos@gmail.com>
2021-02-22 19:07:52 +0800

    apply student auth for profile routes

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,12 +1,9 @@
 var Student = require('../models/student');
 
 // Display details for one student in the /profile route
-exports.student_detail = (req, res) => {
-    console.log('TODO: get data from Student');
-    res.render('profile', {
-        addedStyles: ['profile-styles'],
-        title: 'Profile | Animo.sys',
-        addedScripts: ['profile-script']
-    });
+exports.renderProfilePage = (req, res) => {
+  console.log('TODO: get data from Student');
+  res.render('profile', {
+    title: 'Profile | Animo.sys',
+  });
 };
-
