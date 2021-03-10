5460580fd31836d1a5a70245a2273f6bea17d87b
Rethaniel Ramos <rethanielramos@gmail.com>
2021-03-10 23:22:20 +0800

    fixes

--- models/moderator.js
+++ models/moderator.js
@@ -1,13 +1,1 @@
-const mongoose = require("mongoose");
 
-const moderatorSchema = mongoose.Schema({
-  account: { type: mongoose.Types.ObjectId, ref: "Account", required: true },
-  name: {
-    first: { type: String, required: true },
-    middle: { type: String, required: true },
-    last: { type: String, required: true },
-  },
-  email: { type: String, required: true },
-});
-
-module.exports = mongoose.model("Moderator", moderatorSchema);
