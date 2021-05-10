ba61bd6cda7d4cf09b18d4fea47910d38820143a
Rethaniel Ramos <rethanielramos@gmail.com>
2021-05-10 11:53:04 +0800

    display scholarship in profile page

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,11 +1,25 @@
+const Scholarship = require("../models/Scholarship");
+
 // Display details for one student in the /profile route
 exports.renderProfilePage = async (req, res) => {
-  console.log("TODO: get data from Student");
-  res.render("profile", {
-    title: "Profile | Animo.sys",
-    fname: req.user.name.first,
-    lname: req.user.name.last,
-    mname: req.user.name.middle,
-    idNum: req.user.idNum,
-  });
+  try {
+    let scholarship = { name: "Not a scholar", description: "N/A" };
+
+    if (req.user.scholarship) {
+      scholarship = await Scholarship.findById(req.user.scholarship);
+    }
+
+    res.render("profile", {
+      title: "Profile | Animo.sys",
+      fname: req.user.name.first,
+      lname: req.user.name.last,
+      mname: req.user.name.middle,
+      idNum: req.user.idNum,
+      scholarshipName: scholarship.name,
+      scholarshipDesc: scholarship.description,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
 };
