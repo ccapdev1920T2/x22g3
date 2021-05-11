71d0f0837fea10231eec2f11395b64e4ee992ff1
Rethaniel Ramos <rethanielramos@gmail.com>
2021-05-11 14:15:39 +0800

    change default to false

--- controllers/api/DocumentApiController.js
+++ controllers/api/DocumentApiController.js
@@ -1,32 +1,28 @@
 const Document = require("../../models/Document");
 
-
-exports.sendDocumentRequest =async (req, res) => {
-    console.log(req.user);
-//axios    
-    try{
-
+exports.sendDocumentRequest = async (req, res) => {
+  console.log(req.user);
+  try {
     let document = new Document({
-        requestDate: new Date(),
-        document: req.body["documentType"],
-        isApproved: true,
-        requestedBy: req.user.idNum
+      requestDate: new Date(),
+      document: req.body["documentType"],
+      isApproved: false,
+      requestedBy: req.user.idNum,
     });
     await document.save();
     console.log("BIG BUANCHO");
 
     var details = {
-        msg: "Document requested"
-    }
+      msg: "Document requested",
+    };
     // res.render('degree-process', {
     //     details
     // });
-    res.redirect('/degree-process');
-    
+    res.redirect("/degree-process");
+
     //res.send( await document.save());
     //alert("Succesful request!");
-    }
-    catch(err){
-        console.log(err);
-    }
+  } catch (err) {
+    console.log(err);
+  }
 };
