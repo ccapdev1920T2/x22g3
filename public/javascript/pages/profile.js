316032d59c6b2a4c2d9061849b8ad885a1a0f562
Rethaniel Ramos <rethanielramos@gmail.com>
2021-05-13 19:57:20 +0800

    profile calendar view

--- public/javascript/pages/profile.js
+++ public/javascript/pages/profile.js
@@ -1,51 +1,53 @@
 var studentId = document.getElementById("student-id-hidden");
+var classesNav = document.querySelector(`a[href="#classes"]`);
+var calendar;
 
-// Logic for toggling tabs
+Promise.all([
+  axios.get(`/api/students/${studentId.value}/courses`),
+  axios.get("/api/term-details"),
+])
+  .then(function (response) {
+    var courses = response[0].data;
+    var testTerm = response[1].data.find(
+      (el) => el._id === courses[0].termOffered
+    );
 
-window.location.hash = "#personal-information";
+    var calendarEl = document.getElementById("weekly-schedule");
+    calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: "timeGridWeek",
+      events: toEvent(courses, testTerm),
+      slotMinTime: getEarliestStartTime(courses),
+      slotMaxTime: getLatestEndTime(courses),
+      stickyHeaderDates: false,
+      allDaySlot: false,
+      height: "auto",
+      themeSystem: "bootstrap",
+    });
 
-var listGroupItems = document.querySelector(".list-group").children;
+    calendar.render();
+  })
+  .then(function () {
+    // Logic for toggling tabs
 
-var containers = document.querySelectorAll("div.list-group-target");
+    window.location.hash = "#personal-information";
+    var listGroupItems = document.querySelector(".list-group").children;
+    var containers = document.querySelectorAll("div.list-group-target");
 
-window.onhashchange = setActive;
+    window.onhashchange = function (e) {
+      var hash = window.location.hash;
+      for (let i = 0; i < listGroupItems.length; i++) {
+        var item = listGroupItems.item(i);
+        var container = containers.item(i);
 
-function setActive(e) {
-  var hash = window.location.hash;
-  for (let i = 0; i < listGroupItems.length; i++) {
-    var item = listGroupItems.item(i);
-    var container = containers.item(i);
+        var force = hash === item.hash;
+        item.classList.toggle("active", force);
+        container.classList.toggle("d-none", !force);
+      }
 
-    var force = hash === item.hash;
-    item.classList.toggle("active", force);
-    container.classList.toggle("d-none", !force);
-  }
-}
-
-// calendar
-
-document.addEventListener("DOMContentLoaded", function () {
-  var calendarEl = document.getElementById("weekly-schedule");
-  var calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: "timeGridWeek",
-    events: [{ title: "the title", start: "2021-09-01", end: "2021-09-01" }],
-    stickyHeaderDates: false,
-    allDaySlot: false,
-    height: "auto",
-    themeSystem: "bootstrap",
-  });
-  calendar.render();
-});
-
-// tabulator
-
-// var table = new Tabulator("#tabular-schedule", {
-//   data: sample,
-//   autoColumns: true,
-//   layout: "fitColumns",
-//   pagination: "local",
-//   paginationSize: 10,
-// });
+      calendar.render();
+    };
+  })
+  .catch((err) => console.log(err));
 
 var table = new Tabulator("#tabular-schedule", {
   columns: [
